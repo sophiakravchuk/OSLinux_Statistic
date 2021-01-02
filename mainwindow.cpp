b9ef5ebd456dea7ae3ea2905e16c5fe3a260d733
sophiakravchuk <47211843+sophiakravchuk@users.noreply.github.com>
2021-01-02 19:05:59 +0200

    Add documentation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -35,12 +35,13 @@ MainWindow::MainWindow(QWidget *parent)
     ui(new Ui::MainWindow)
     {
         ui->setupUi(this);
-
+        
+        // creation of the database
         db = QSqlDatabase::addDatabase("QSQLITE");
         std::string curr_path = std::filesystem::temp_directory_path();
         std::string path_to_db = curr_path + "/db.sqlite";
         if(std::filesystem::exists(path_to_db)){
-            std::filesystem::remove(path_to_db);
+            std::filesystem::remove(path_to_db); // delete old database
         }
 
         db.setDatabaseName(path_to_db.c_str());
@@ -71,49 +72,58 @@ MainWindow::MainWindow(QWidget *parent)
                                          QMessageBox::Ok);
             qApp->quit();
         }
-
+        
+        //get the constant information 
         std::map<std::string, std::string> consti = get_constant_info();
         constinf.cpu_cores = stoi(consti["cpu cores"]);
         constinf.cpu_name = consti["model name"];
 
+        // create timer that will call update_database() with the delay
         wind_timer = new QTimer(this);
         connect(wind_timer, &QTimer::timeout, this, &MainWindow::update_database);
         wind_timer->start(1000);
 
+        // edit table of processes, connect titles to function
         ui->tableWidget->verticalHeader()->setVisible(false);
-
         QObject::connect( ui->tableWidget->horizontalHeader(), SIGNAL( sectionClicked( int ) ), this, SLOT( on_myTable_sectionClicked( int ) ) );
 
+        // add processes icon on the processes table
         QPixmap pixmap(":/processes.png");
         QIcon ButtonIcon(pixmap);
         ui->Processes_Button->setIcon(ButtonIcon);
         ui->Processes_Button->setIconSize(QSize(20, 20));
 
+        // add cpu icon on the processes table
         QPixmap pixmap1(":/cpu.png");
         QIcon ButtonIcon1(pixmap1);
         ui->CPU_Button->setIcon(ButtonIcon1);
         ui->CPU_Button->setIconSize(QSize(20, 20));
-
+        
+        // add memory icon on the processes table
         QPixmap pixmap2(":/memory.png");
         QIcon ButtonIcon2(pixmap2);
         ui->Memory_Button->setIcon(ButtonIcon2);
         ui->Memory_Button->setIconSize(QSize(20, 20));
 
+        // add about us icon on the processes table
         QPixmap pixmap3(":/aboutus.png");
         QIcon ButtonIcon3(pixmap3);
         ui->AboutUs_Button->setIcon(ButtonIcon3);
         ui->AboutUs_Button->setIconSize(QSize(20, 20));
 
+        // add help icon on the processes table
         QPixmap pixmap4(":/help.png");
         QIcon ButtonIcon4(pixmap4);
         ui->Help_Button->setIcon(ButtonIcon4);
         ui->Help_Button->setIconSize(QSize(25, 25));
 
+        // add dark_theme icon on the processes table
         QPixmap pixmap5(":/dark_theme.png");
         QIcon ButtonIcon5(pixmap5);
         ui->DarkTheme->setIcon(ButtonIcon5);
         ui->DarkTheme->setIconSize(QSize(25, 25));
 
+        // create buttons list
         btns.push_back(ui->Processes_Button);
         btns.push_back(ui->CPU_Button);
         btns.push_back(ui->Memory_Button);
@@ -121,22 +131,32 @@ MainWindow::MainWindow(QWidget *parent)
         btns.push_back(ui->Help_Button);
         btns.push_back(ui->DarkTheme);
 
+        // create all the graphs
         ui->plot->addGraph();
         ui->plot_2->addGraph();
         ui->plot_3->addGraph();
         ui->plot_4->addGraph();
+        
+        // create graphs list
         plts.push_back(ui->plot);
         plts.push_back(ui->plot_2);
         plts.push_back(ui->plot_3);
         plts.push_back(ui->plot_4);
+        
+        // get first /proc information
         update_database();
+        
+        //style text fields
         style_text_fields();
+        
+        //set default colors
         theme_highlight_color = "#90e0ef";
         theme_default_color = "#efefef";
         theme_text_color = "#000000";
         std::string background_color = "background-color:" + theme_default_color + ";";
         this->setStyleSheet(QString::fromStdString(background_color));
-
+        
+        //render first page
         on_Processes_Button_clicked();
     }
 
@@ -147,6 +167,9 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::style_text_fields(){
+    /**
+    * @brief This function sets styleSheet of all the text fields in the application.
+    */
     std::string text_c = "color:'" + theme_text_color + "';";
     std::string cpu_text_c = "background-color:'#289EE37D'; " + text_c;
     std::string mem_text_c = "background-color:'#229D4EDD'; " + text_c;
@@ -174,10 +197,19 @@ void MainWindow::style_text_fields(){
 };
 
 void MainWindow::update_database(){
+    /**
+    * @brief This function reads the /proc filesystem and gets information
+    * about all the processes. It fills all_task_info and insert data to 
+    * the database.
+    */
+    // clear old information to fill new one
     all_tasks_info.clear();
+    
     long mem_total = get_mem_total();
     time_t now = time(0);
-    if (pushed_time_checkpoints >= 100){
+    
+    if (pushed_time_checkpoints >= 100){ // check whether we have not more than 100 different timepoints
+        // if there are more than 100 different timepoints (the information older than 100 seconds) we delete it
         QString query = "DELETE FROM all_finfos WHERE time_checkp = (SELECT MIN(time_checkp) FROM all_finfos);";
         QSqlQuery qry(db);
         if(!qry.exec(query)){
@@ -200,7 +232,7 @@ void MainWindow::update_database(){
     QVariantList psss;
     QVariantList memusages;
 
-    for(auto &p : std::filesystem::directory_iterator("/proc/")){
+    for(auto &p : std::filesystem::directory_iterator("/proc/")){ // read /proc and fill all_tasks_info
         std::string file_path =p.path();
         struct task_manager_file_info finfo;
         std::vector<std::string> vec = split(file_path, "/");
@@ -253,7 +285,10 @@ void MainWindow::update_database(){
 }
 
 void MainWindow::render_window() {
-
+    /**
+    * @brief This function checks what page should be drawn now and 
+    * calls needed functions.
+    */
     if (actv_wind.processes) {
         update_table();
     }
